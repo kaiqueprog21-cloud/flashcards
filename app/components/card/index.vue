136326b766407cb381e9a9c4426e5185a961a0c2
daniel miranda <danielmiranda132@yahoo.com>
2026-07-10 16:21:48 -0300

    create card component

---
+++ app/components/card/index.vue
@@ -0,0 +1,18 @@
+<style scoped>
+    @import "./style.css";
+</style>
+
+<script setup>
+    defineProps({
+        title: String,
+        value: Number
+    })
+</script>
+
+<template>
+    <div class="card">
+        <img src="/icons/icone-card.svg"/>
+        <p class="card-title">{{ title }}</p>
+        <p class="card-value">{{ value }}</p>
+    </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,17 @@
+<style scoped>
+    @import "./style.css";
+</style>
+<script>
+    import Card from '@/components/card'
+</script>
 <template>
     <div class="container">
         <h1>Hello, Alex!</h1>
         <p>You're on a 12-day streak. Keep the momentum going!</p>
+
+        <div class="cards-container">
+            <Card title="Active Sets" value="8" />
+            <Card title="Mastered" value="412" />
+        </div>
     </div>
 </template>
