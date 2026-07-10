eee971337d98730334917f9d309a5194982effba
daniel miranda <danielmiranda132@yahoo.com>
2026-07-10 16:02:47 -0300

    create layout

--- app/app.vue
+++ app/app.vue
@@ -3,14 +3,8 @@
   @import './style.css';
 </style>
 
-<script>
-  import Header from "./components/header"
-</script>
-
 <template>
-  <Header/>
-  <div class="container">
-    <h1>Hello, Alex!</h1>
-    <p>You're on a 12-day streak. Keep the momentum going!</p>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container">
+        <h1>Hello, Alex!</h1>
+        <p>You're on a 12-day streak. Keep the momentum going!</p>
+    </div>
+</template>
