9f7accab902490e8537429ed18e06145e21bc779
KaiqueS <kaiqueprog21@gmail.com>
2026-07-14 16:18:05 -0300

    new cards page

---
+++ app/components/input/index.vue
@@ -0,0 +1,23 @@
+<style scoped>
+    @import "./style.css";
+</style>
+
+<script setup>
+   import { ref } from 'vue'
+
+    const frontText = ref('')
+    defineProps({
+        title: String,
+
+        placeholder: {
+            type: String,
+            default: 'Enter the question or concept...'
+        }
+    })
+</script>
+
+<template>
+    <h3 class="front-side">{{ title }}</h3>  
+    <textarea v-model="frontText" class="front-text" maxlength="200" rows="4" cols="20" :placeholder="placeholder"></textarea>
+    <span>{{ frontText.length }}/200</span>
+</template>
---
+++ app/pages/new-card.vue
@@ -0,0 +1,38 @@
+<style scoped>
+@import './style.css';
+</style>
+<script setup>
+    import Input from '@/components/input';
+    import { ref } from 'vue'
+
+   const fileInput = ref(null)
+   const selectedImage = ref(null)
+
+    function openfileselector() {
+        fileInput.value.click()
+    }
+
+    function handleFileSelected(event) {
+        const file = event.target.files[0]
+        selectedImage.value = file
+        console.log(file)
+    }
+
+</script>
+<template>
+    <h2 class="new-card">Create a new card</h2> 
+    <p class="texts">Draft your card with precision. Add images to boost recall.</p>
+        <Input title="FRONT SIDE" placeholder="Enter the question or concept..."/> 
+        <Input title="BACK SIDE" placeholder="Enter the answer or detailed explanation..."/> 
+    <h3 class="association">VISUAL ASSOCIATION</h3>
+        <div class="files" @click="openfileselector">
+            <input type="file" ref="fileInput" accept=".png,.jpg,.jpeg" style="display: none" @change="handlefileselected"/>
+            <div class="upload-content">
+                <img src="/icons/icone-image.svg"/>
+                <p>Click or drop image here</p>
+                <span>PNG , JPG up to 10MB</span>
+            </div>
+        </div>
+    <button class="save">Save New Card</button>  
+    <button class="cancel">Cancel</button>  
+</template>    
